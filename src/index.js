1662d978c71c097e25acc79d47013e644b166520
Oluwatobi <emmanuelloluwatobi@gmail.com>
2021-09-10 01:49:33 +0100

    fully ready for production

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ const app = express();
 //add cors middleware
 app.use(cors())
 
+//event url
 const eventUrl =
   "https://www.googleapis.com/calendar/v3/calendars/primary/events";
 
 app.get("/", async (req, res) => {
   try {
+    //check if authorization header is available
     const { authorization } = req.headers;
     if (!authorization) {
       return res.json({
@@ -18,30 +20,36 @@ app.get("/", async (req, res) => {
         message: "pls provide authorization token",
       });
     }
+    //make a request to google calneder server to get clander with user authorization token
     const resp = await axios.get(eventUrl, {
       headers: {
         authorization: `Bearer ${authorization}`,
       },
     });
+    //destructure items from response data
     const { items } = resp.data;
+    //map through items to filter needful information
     const data = items.map((item) => ({
       title: item.summary,
       description: item.description,
       start: item.start?.dateTime,
       url: item.location,
     }));
-
+   //return response
     return res.json({ data });
   } catch (error) {
+    //log error if occured
     console.log({error})
+    //return 400 respomse
     return res
       .status(400)
       .json({ status: "error", message: "error occured pls try again" });
   }
 });
 
+//setting port
 const port = process.env.PORT || 9090;
-
+// creaing server with express
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
